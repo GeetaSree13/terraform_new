5eedb41fbe640e90d00cafea772edee7bcca87e2
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-06-28 18:37:32 +0530

    added

--- project2/backend.tf
+++ project2/backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "gcs" {
     bucket  = "tfstate-bucket"
     prefix  = "project2/terraform/state"
+    credentials = "/home/atlantis/.atlantis/repos/GeetaSree13/terraform_new/creds2.json"
   }
 }
