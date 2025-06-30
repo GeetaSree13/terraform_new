1fba31dd0774841f76d6af8a97979903e805467c
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-06-30 19:13:17 +0530

    added

--- project2/backend.tf
+++ project2/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket  = "my-bucket-project2"
-    prefix  = "project2/terraform/state"
-    #credentials = "/gcp-creds/creds2.json"
+    bucket      = "my-bucket-project2"
+    prefix      = "project2/terraform/state"
+    credentials = "/gcp-creds/creds2.json"
   }
 }
-
-###
