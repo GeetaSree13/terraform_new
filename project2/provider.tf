7727764b628422169fd8f5f6c441282f73f044ea
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-06-30 12:23:02 +0530

    added

--- project2/provider.tf
+++ project2/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("/home/atlantis/.atlantis/repos/GeetaSree13/terraform_new/3/default/creds2.json")
+  credentials = file("/gcp-creds/creds2.json")
   project     = var.project_id
   region      = var.region
 }
