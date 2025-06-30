f8255564c0d21b959e1631b9bf9bfae13dabec7a
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-06-30 18:42:40 +0530

    added

--- project1/provider.tf
+++ project1/provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = file("/gcp-creds/creds.json")
   project     = var.project_id
   region      = var.region
 }
--- project2/provider.tf
+++ project2/provider.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = file("/gcp-creds2/creds.json")
   project     = "stable-parsec-462607-h1"
   region      = "asia-southeast1"
 }
