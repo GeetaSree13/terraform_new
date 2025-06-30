deba681a641dc1fbc62c6181a528f6362ae6cd09
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-06-30 11:20:32 +0530

    added

--- project1/provider.tf
+++ project1/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("/home/atlantis/.atlantis/repos/GeetaSree13/terraform_new/creds.json")
+  credentials = file("/home/atlantis/.atlantis/repos/GeetaSree13/terraform_new/3/default/creds.json")
   project     = var.project_id
   region      = var.region
 }
